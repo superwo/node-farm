d2b25568cf3ac750d3f37d7dfcad76cd157fae4d
Radu <avadanii@tiscali.it>
2019-06-26 12:29:04 +0300

    simple server

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requrests on port 8000');
+});
